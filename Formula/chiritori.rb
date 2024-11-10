efb8244597d424dc10ade974645e626585428e58
piyoppi <garakuta.toolbox@gmail.com>
2024-11-10 16:04:56 +0900

    Chiritori v1.1.0 released

--- Formula/chiritori.rb
+++ Formula/chiritori.rb
@@ -1,30 +1,30 @@
 class Chiritori < Formula
   desc "Tool for removing time-limited source code"
   homepage "https://github.com/piyoppi/chiritori"
-  version "1.0.0"
+  version "1.1.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/piyoppi/chiritori/releases/download/v1.0.0/chiritori-darwin-arm64.tar.gz"
-      sha256 "0ab440d132569c234cae5f96122e22e184fb72c87128c4dbfc98102f2359bb95"
+      url "https://github.com/piyoppi/chiritori/releases/download/v1.1.0/chiritori-darwin-arm64.tar.gz"
+      sha256 "b768934946364a5826b03c9286e12b91523ff5d8df54dcc2bf37d09b4727cd38"
     end
 
     on_intel do
-      url "https://github.com/piyoppi/chiritori/releases/download/v1.0.0/chiritori-darwin-x86_64.tar.gz"
-      sha256 "f7e07199ec4c40087d9f76bdf2a26792cf9b78f15e71e3a215bf93ee7abfe18d"
+      url "https://github.com/piyoppi/chiritori/releases/download/v1.1.0/chiritori-darwin-x86_64.tar.gz"
+      sha256 "849ecfff58281af630e7d8b8ce0e25bdeaaa13ebc04631336e1ca0bb651c4868"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/piyoppi/chiritori/releases/download/v1.0.0/chiritori-linux-arm64-musl.tar.gz"
-      sha256 "a343d8476b248e68ba246aa52a6943955c57cde61b37ba27e2438e9b78042158"
+      url "https://github.com/piyoppi/chiritori/releases/download/v1.1.0/chiritori-linux-arm64-musl.tar.gz"
+      sha256 "b34441fcd2b470708779c41d8f66f3d11899a0c913c31415e6923d9c57590e89"
     end
 
     on_intel do
-      url "https://github.com/piyoppi/chiritori/releases/download/v1.0.0/chiritori-linux-x86_64-musl.tar.gz"
-      sha256 "4981aeb3f84709e0f5c14c9a8648cc307dcca52a9119cf42068320a27f65f3dd"
+      url "https://github.com/piyoppi/chiritori/releases/download/v1.1.0/chiritori-linux-x86_64-musl.tar.gz"
+      sha256 "fce378eff522c4dfd3664f90bc65d9105ccc8b17c725c895d299cd1d07db6110"
     end
   end
 
