999508bb6bf1dbac9d247e3f7aa437cdad3a6463
piyoppi <garakuta.toolbox@gmail.com>
2024-11-17 12:12:07 +0900

    Fix chiritori v1.2.0

--- Formula/chiritori.rb
+++ Formula/chiritori.rb
@@ -7,24 +7,24 @@ class Chiritori < Formula
   on_macos do
     on_arm do
       url "https://github.com/piyoppi/chiritori/releases/download/v1.2.0/chiritori-darwin-arm64.tar.gz"
-      sha256 "3fafc73577121a6a54ceec628d32c048292a01eadcfc5ce4979f075228635a8c"
+      sha256 "a2afff8da299739204f13a699c7462e56712590fc15eaf265b099290a24ceb21"
     end
 
     on_intel do
       url "https://github.com/piyoppi/chiritori/releases/download/v1.2.0/chiritori-darwin-x86_64.tar.gz"
-      sha256 "54be06d8b0d8d77869999ec36a9a0e0b8369c60ea01e80d52e2dfab68ae1ffe5"
+      sha256 "8bd64aa644f6f10002b833b199ac29cd36626cc4e2fca2382b93b12a71b7f5ab"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/piyoppi/chiritori/releases/download/v1.2.0/chiritori-linux-arm64-musl.tar.gz"
-      sha256 "693883e068dccf1d264073da5eb394a7c346730be5e3ef36be6ac8aa04969145"
+      sha256 "7d0e701b660924a68d57d76fca8e7c524b9842564520c960fb41f230e377ff64"
     end
 
     on_intel do
       url "https://github.com/piyoppi/chiritori/releases/download/v1.2.0/chiritori-linux-x86_64-musl.tar.gz"
-      sha256 "7899650ed325e05ac57348b1e72bd4fd082cc41cc263eb219a0a794d626dbf2a"
+      sha256 "dfaef373d016018d00ac87023348ce0e76eedd1c5d22071f51aebf8785d82f8b"
     end
   end
 
