92ea643aed536c91916b26f013605220d36ea3de
piyoppi <garakuta.toolbox@gmail.com>
2026-03-14 14:22:42 +0900

    update chiritori v1.5.0

--- Formula/chiritori.rb
+++ Formula/chiritori.rb
@@ -1,30 +1,30 @@
 class Chiritori < Formula
   desc "Tool for removing time-limited source code"
   homepage "https://github.com/piyoppi/chiritori"
-  version "1.4.1"
+  version "1.5.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/piyoppi/chiritori/releases/download/v1.4.1/chiritori-darwin-arm64.tar.gz"
-      sha256 "a6063c48df9b535dae20eb028517c968b5e1e786db8036af21b651bfffbc9eca"
+      url "https://github.com/piyoppi/chiritori/releases/download/v1.5.0/chiritori-darwin-arm64.tar.gz"
+      sha256 "7df1d308f815edcc7c2557a37d6affa8900053e9750da1a3516a8f387e78a54d"
     end
 
     on_intel do
-      url "https://github.com/piyoppi/chiritori/releases/download/v1.4.1/chiritori-darwin-x86_64.tar.gz"
-      sha256 "b099369286b2ab87af5633db9effd7f40f5eaf7792c1c7fe3b3e381612c01ee9"
+      url "https://github.com/piyoppi/chiritori/releases/download/v1.5.0/chiritori-darwin-x86_64.tar.gz"
+      sha256 "25c6baca6ec5b74bac151d29dd4bb69b97533db6ca1282b044776fdbfcc3f59b"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/piyoppi/chiritori/releases/download/v1.4.1/chiritori-linux-arm64-musl.tar.gz"
-      sha256 "91f4e44d915c8f4d8f4c7e4f9bafe47c8b92882e9242133f5bf8e1f7b7c243be"
+      url "https://github.com/piyoppi/chiritori/releases/download/v1.5.0/chiritori-linux-arm64-musl.tar.gz"
+      sha256 "522260ffa0776e1eaeda7d169e9c9e9c5529f055c37edeb70b203591d62e0171"
     end
 
     on_intel do
-      url "https://github.com/piyoppi/chiritori/releases/download/v1.4.1/chiritori-linux-x86_64-musl.tar.gz"
-      sha256 "6b225243bd163e8390ed05af043dcad33f49b9711b0cfa90554cc713595db097"
+      url "https://github.com/piyoppi/chiritori/releases/download/v1.5.0/chiritori-linux-x86_64-musl.tar.gz"
+      sha256 "aa79f908f581c39b28ebb7f243504a0372860941dce2c2988b3b773ada3b738e"
     end
   end
 
